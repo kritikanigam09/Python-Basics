f815b3542b501d5c8e167d89eb357cf3aad9b183
kritikanigam09 <kritikanigam09@gmail.com>
2018-05-27 15:20:17 -0400

    More commits

--- Ifexamples.py
+++ Ifexamples.py
@@ -7,8 +7,8 @@ def using_control_again():
  if 45<99:
   return "Success #2"
   
-print using_control_once
-print using_control_again
+print(using_control_once())
+print(using_control_again())
 
 
 
@@ -20,10 +20,32 @@ def greater_less_equal_5(answer):
  else:
   return 0
 
-print greater_less_equal_5(4)
-print greater_less_equal_5(5)
-print greater_less_equal_5(6)
+print(greater_less_equal_5(4))
+print(greater_less_equal_5(5))
+print(greater_less_equal_5(6))
 
 # output: -1 , 0, 1
+
+if 8>9:
+  print("I don't get printed!")
+else:
+  print("I get printed!")
+
+
+  def grade_converter(grade):
+    if grade>=90:
+      return "A"
+    elif grade>=80 and grade<=89:
+      return "B"
+    elif grade>=70 and grade<=79:
+      return "C"
+    elif grade>=65 and grade<=69:
+      return"D"
+    else:
+      return "F"
+
+    print (grade_converter(92))
+    print (grade_converter(70))
+    print (grade_converter(61))
 
 
